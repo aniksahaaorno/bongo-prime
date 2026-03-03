ee4a1d2379e35fab4661b34dd4a469d2f4788a44
gmitsolution3 <gmitsolution3@gmail.com>
2026-03-03 10:07:39 +0600

    changed thankyou page text

--- app/(public)/thank-you/page.tsx
+++ app/(public)/thank-you/page.tsx
@@ -41,7 +41,7 @@ export default function ThankYou() {
         </div>
 
         <h1 className="text-3xl font-bold mb-2">
-          Payment Successful 🎉
+          Order Successful 🎉
         </h1>
 
         <p className="text-gray-600 mb-8">
